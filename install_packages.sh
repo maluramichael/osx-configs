42029ba8de3b4fe9681d0a2bc505874872effd16
Michael Malura <maluramichael@googlemail.com>
2016-10-17 10:32:52 +0200

    Add git

--- install_packages.sh
+++ install_packages.sh
@@ -5,8 +5,8 @@ echo 'install brew...'
 
 # brew development tools
 echo 'brew development tools...'
-brew install nvm
-brew cask install mysqlworkbench iterm2 atom webstorm dash macdown paw
+brew install git nvm
+brew cask install rbenv mysqlworkbench iterm2 atom webstorm dash macdown paw
 
 # brew web
 echo 'brew web...'
@@ -15,7 +15,7 @@ brew cask install jdownloader dropbox google-chrome hipchat skype teamviewer
 # brew system
 echo 'brew system...'
 brew install ffmpeg
-brew cask install whatpulse adapter blender istat-menus alfred shuttle vlc
+brew cask install whatpulse adapter blender istat-menus alfred shuttle vlc 1password
 
 # install other stuff from app store
 # window tidy
@@ -24,3 +24,10 @@ brew cask install whatpulse adapter blender istat-menus alfred shuttle vlc
 # affinity designer
 # affinity photo
 # airmail
+
+echo 'nvm...'
+nvm install stable
+nvm use stable
+
+echo 'node...'
+node install -g nodemon
