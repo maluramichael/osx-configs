67e9ee1cc727dd4e0ec452b4a35d36d137d96e5d
Michael Malura <maluramichael@googlemail.com>
2016-11-04 13:46:56 +0100

    Add osx preferences script

---
+++ setup_osx_preferences.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+#Set a shorter Delay until key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 5
+
+#Disable mouse acceleration
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+#Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+#Show the ~/Library folder
+chflags nohidden ~/Library
+
+#Store screenshots in subfolder on desktop
+mkdir ~/Desktop/Screenshots
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+#Setup Git
+ssh-keygen -t rsa -C "maluramichael@googlemail.com"
+
+# Test connection
+ssh -T git@github.com
+
+# Set git config values
+git config --global user.name "Michael Malura"
+git config --global user.email "maluramichael@googlemail.com"
+git config --global github.user maluramichael
+git config --global github.token YOUR_TOKEN_HERE
+git config --global color.ui true
+
+#Restart Finder
+sudo pkill Finder
