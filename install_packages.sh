33505fa52557395d6958962ad39dfc85f2ccdbdc
Michael Malura <maluramichael@googlemail.com>
2017-03-01 18:17:28 +0100

    Reorder packages

--- install_packages.sh
+++ install_packages.sh
@@ -3,6 +3,36 @@ echo 'install brew...'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 
+echo 'brew system...'
+brew install zsh zsh-completions
+chsh -s /bin/zsh $USER
+
+brew install watch
+brew install wget
+brew install imagemagick
+brew install graphicsmagick
+brew install watchman
+brew install speedtest_cli
+brew install nmap
+brew install htop
+brew install unrar
+brew install irssi
+brew install rename
+brew install trash
+brew cask install whatpulse
+brew cask install adapter
+brew cask install blender
+brew cask install istat-menus
+brew cask install alfred
+brew cask install shuttle
+brew cask install vlc
+brew cask install 1password
+brew cask install cakebrew
+brew cask install charles
+brew cask install love
+brew cask install filezilla
+brew cask install spotify
+
 echo 'brew development tools...'
 brew install git
 brew install git-flow
@@ -24,7 +54,9 @@ brew install jq
 brew install hub
 brew install httpie
 brew install xmake
+brew install ffmpeg --with-fdk-aac --with-fontconfig --with-freetype --with-frei0r --with-libass --with-libbluray --with-libbs2b --with-libcaca --with-libebur128 --with-libsoxr --with-libssh --with-libtesseract --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openh264 --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-rubberband --with-schroedinger --with-sdl2 --with-snappy --with-speex --with-tesseract --with-theora --with-tools --with-webp --with-x265 --with-xz --with-zeromq --with-zimg
 
+brew cask install java
 brew cask install iterm2
 brew cask install atom
 brew cask install webstorm
@@ -47,70 +79,22 @@ brew cask install skype
 brew cask install teamviewer
 brew cask install gitter
 
-echo 'brew system...'
-brew install ffmpeg --with-fdk-aac --with-fontconfig --with-freetype --with-frei0r --with-libass --with-libbluray --with-libbs2b --with-libcaca --with-libebur128 --with-libsoxr --with-libssh --with-libtesseract --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openh264 --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-rubberband --with-schroedinger --with-sdl2 --with-snappy --with-speex --with-tesseract --with-theora --with-tools --with-webp --with-x265 --with-xz --with-zeromq --with-zimg
-brew install watch
-brew install wget
-brew install imagemagick
-brew install graphicsmagick
-brew install watchman
-brew install speedtest_cli
-brew install nmap
-brew install htop
-brew install lftp
-brew install unrar
-brew install irssi
-brew install rename
-brew install trash
-
-brew cask install whatpulse
-brew cask install adapter
-brew cask install blender
-brew cask install istat-menus
-brew cask install alfred
-brew cask install shuttle
-brew cask install vlc
-brew cask install 1password
-brew cask install cakebrew
-brew cask install charles
-brew cask install love
-brew cask install filezilla
-brew cask install spotify
-
-# install other stuff from app store
+# echo 'app store...'
 # window tidy
 # xcode
 # reeder
 # affinity designer
 # affinity photo
 # airmail
+# moneymoney
 
-echo 'nvm...'
-nvm install stable
-nvm use stable
+echo 'games...'
+brew cask install steam
+brew cask install battle-net
+open -a /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
 
-echo 'node...'
-npm install -g nodemon
-npm install -g yarn
-npm install -g react-native-cli
-npm install -g reactotron-cli
-
-echo 'rbenv...'
-rbenv install 2.3.0
-rbenv global 2.3.0
-
-echo 'gems...'
-gem install cocoapods
-gem install jekyll
-
-echo 'python 2...'
-cd $HOME/Temp
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-
-echo 'virtualenv...'
-sudo pip3 install virtualenv --upgrade
-pip3 install http-prompt
+brew cask install gog-galaxy
+brew cask install league-of-legends
 
 echo 'reset...'
 cd $HOME
