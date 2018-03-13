b2627771c99c4bb3a51bab1620a1bf7ce358d16c
Michael Malura <github@malura.me>
2018-03-13 20:27:42 +0100

    Add keystore environment variable

--- .shell/exports.sh
+++ .shell/exports.sh
@@ -1,13 +1,14 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 export LANG=en_US.UTF-8
-export DEV_HOME="~/Development"
+export DEV_HOME="$HOME/development"
 export REACT_EDITOR=code
 export NVM_DIR="$HOME/.nvm"
 
 # android
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export ANDROID_BUILD_TOOLS="$ANDROID_HOME/build-tools/$(ls -t $ANDROID_HOME/build-tools | tail -1)"
+export ANDROID_KEYSTORES="$DEV_HOME/keystores"
 export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_BUILD_TOOLS"
 
 if [ -f ~/.credentials.sh ]; then
