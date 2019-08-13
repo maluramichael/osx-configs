8935e88bf40fccb4cad7c60db7a334e726a7505b
Michael Malura <github@malura.me>
2019-08-13 16:18:31 +0200

    Add zsh plugins

--- .zshrc
+++ .zshrc
@@ -1,7 +1,13 @@
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="devnetik"
 COMPLETION_WAITING_DOTS="true"
-plugins=(git docker)
+plugins=(
+ git
+ docker
+ debian
+ zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-completions
+)
+autoload -U compinit && compinit
 
 source $ZSH/oh-my-zsh.sh
 
--- install_packages.sh
+++ install_packages.sh
@@ -83,6 +83,12 @@ brew cask install firefox
 # airmail
 # moneymoney
 
+echo 'zsh plugins...'
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
 echo 'games...'
 brew cask install steam
 brew cask install battle-net
