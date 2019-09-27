cc08e55709ba1c8d1d225219f32d0f1774d37758
Michael Malura <github@malura.me>
2019-09-27 09:32:59 +0200

    Add virtualenv exports

--- .shell/exports.sh
+++ .shell/exports.sh
@@ -6,10 +6,15 @@ export REACT_EDITOR=code
 export NVM_DIR="$HOME/.nvm"
 
 # android
-export ANDROID_HOME="$HOME/Library/Android/sdk"
-export ANDROID_BUILD_TOOLS="$ANDROID_HOME/build-tools/$(ls -t $ANDROID_HOME/build-tools | tail -1)"
-export ANDROID_KEYSTORES="$DEV_HOME/keystores"
-export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_BUILD_TOOLS"
+#export ANDROID_HOME="$HOME/Library/Android/sdk"
+#export ANDROID_BUILD_TOOLS="$ANDROID_HOME/build-tools/$(ls -t $ANDROID_HOME/build-tools | tail -1)"
+#export ANDROID_KEYSTORES="$DEV_HOME/keystores"
+#export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_BUILD_TOOLS"
+
+# virtualenv
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/development/projects
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 
 export CARGO_HOME="$HOME/.cargo"
 export PATH="$PATH:$CARGO_HOME/bin"
