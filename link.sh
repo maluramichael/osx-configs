05f4bdb54d0988c2c4cfdd97ba2fc0289484cd03
Michael Malura <github@malura.me>
2019-10-26 16:28:28 +0200

    Fix link script

--- link.sh
+++ link.sh
@@ -1,12 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-currentDirectory=$(pwd)
-cd $HOME
+echo "Link from $PWD to $HOME"
 
-mkdir -p .oh-my-zsh/themes
+# ZSH
+if test -f "$HOME/.zshrc" && ! test -h "$HOME/.zshrc"; then mv "$HOME/.zshrc" "$HOME/.zshrc.bak"; fi
+if ! test -f "$HOME/.zshrc"; then ln -s "$PWD/.zshrc" "$HOME/.zshrc"; fi
 
-ln -sf "$currentDirectory/.shell" .shell
-ln -sf "$currentDirectory/.zshrc" .zshrc
-ln -sf "$currentDirectory/.npmrc" .npmrc
-ln -sf "$currentDirectory/.gitconfig" .gitconfig
-ln -sf "$currentDirectory/.oh-my-zsh/themes/devnetik.zsh-theme" ".oh-my-zsh/themes/devnetik.zsh-theme"
+mkdir -p "$HOME/.oh-my-zsh/themes"
+if ! test -h "$HOME/.oh-my-zsh/themes/devnetik.zsh-theme"; then
+  ln -s "$PWD/.oh-my-zsh/themes/devnetik.zsh-theme" "$HOME/.oh-my-zsh/themes/devnetik.zsh-theme"
+fi
+
+# Bash
+if test -f "$HOME/.profile" && ! test -h "$HOME/.profile"; then mv "$HOME/.profile" "$HOME/.profile.bak"; fi
+if ! test -f "$HOME/.profile"; then ln -s "$PWD/.profile" "$HOME/.profile"; fi
+
+# NPM
+if test -f "$HOME/.npmrc" && ! test -h "$HOME/.npmrc"; then mv "$HOME/.npmrc" "$HOME/.npmrc.bak"; fi
+if ! test -f "$HOME/.npmrc"; then ln -s "$PWD/.npmrc" "$HOME/.npmrc"; fi
+
+# GIT
+if test -f "$HOME/.gitconfig" && ! test -h "$HOME/.gitconfig"; then mv "$HOME/.gitconfig" "$HOME/.gitconfig.bak"; fi
+if ! test -f "$HOME/.gitconfig"; then ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"; fi
+
+# CONKY
+if test -f "$HOME/.conkyrc" && ! test -h "$HOME/.conkyrc"; then mv "$HOME/.conkyrc" "$HOME/.conkyrc.bak"; fi
+if ! test -f "$HOME/.conkyrc"; then ln -s "$PWD/.conkyrc" "$HOME/.conkyrc"; fi
+
+if [ -d "$HOME/.conky.d" ]; then
+  if ! test -h "$HOME/.conky.d"; then
+    mv "$HOME/.conky.d" "$HOME/.conky.d.bak/"
+  fi
+fi
+if [ ! -d "$HOME/.conky.d" ]; then
+  ln -s "$PWD/.conky.d" "$HOME/.conky.d"
+fi
+
+# SHELL
+if [ -d "$HOME/.shell" ]; then
+  if ! test -h "$HOME/.shell"; then
+    mv "$HOME/.shell" "$HOME/.shell.bak/"
+  fi
+fi
+if [ ! -d "$HOME/.shell" ]; then
+  ln -s "$PWD/.shell" "$HOME/.shell"
+fi
