a366dc52c7b5b01b77fa67fad7369ad18ce3270a
Michael Malura <github@malura.me>
2019-10-26 16:31:02 +0200

    Minor changes

--- install_packages.sh
+++ install_packages.sh
@@ -4,8 +4,6 @@ echo 'install brew...'
 brew install caskroom/cask/brew-cask
 
 echo 'brew system...'
-brew install zsh zsh-completions
-
 brew install $(<brew_packages.txt)
 brew cask install $(<brew_cask_packages.txt)
 
@@ -21,7 +19,7 @@ chsh -s /bin/zsh $USER
 # moneymoney
 
 echo 'zsh plugins...'
-git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
@@ -29,10 +27,9 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zs
 echo 'games...'
 brew cask install steam
 brew cask install battle-net
-open -a /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
-
 brew cask install gog-galaxy
-brew cask install league-of-legends
+
+open -a /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
 
 echo 'rust...'
 curl https://sh.rustup.rs -sSf | sh
